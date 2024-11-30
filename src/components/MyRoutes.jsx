bd614678569c3f74086242fb9ed8e2e318786425
Sean O'Connor <sfoconnor@Seans-MacBook-Pro-2.local>
2024-11-29 23:32:41 -0600

    Trying to fix routing when deployed publicly

--- src/components/MyRoutes.jsx
+++ src/components/MyRoutes.jsx
@@ -3,12 +3,12 @@ import NFL from "./Bodies/NFL"
 import NBA from "./Bodies/NBA"
 import MLB from "./Bodies/MLB"
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 
 export default function MyRoutes(){
     return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route index element={<Home />} />
             <Route path= "Home" element={<Home />} />
@@ -17,5 +17,5 @@ export default function MyRoutes(){
             <Route path="MLB" element={<MLB />} />
             <Route path="*" element={<p>Route Wrong</p>} />
         </Routes>
-    </BrowserRouter>);
+    </HashRouter>);
 }
